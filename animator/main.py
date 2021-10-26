4be8f7b92e7732b14982abfd94773333ce44a266
louiseslb <louise.saliba99@gmail.com>
2021-10-26 12:40:25 +0200

    import file

--- animator/main.py
+++ animator/main.py
@@ -1,29 +1,4 @@
-import pygame
-import random
-from pygame.locals import *
-
-
-# function that allows to run OpenGL on MacOS
-def monkeypatch_ctypes():
-    import os
-    import ctypes.util
-    uname = os.uname()
-    if uname.sysname == "Darwin" and uname.release >= "20.":
-        real_find_library = ctypes.util.find_library
-
-        def find_library(name):
-            if name in {"OpenGL", "GLUT"}:  # add more names here if necessary
-                return f"/System/Library/Frameworks/{name}.framework/{name}"
-            return real_find_library(name)
-
-        ctypes.util.find_library = find_library
-    return
-
-
-monkeypatch_ctypes()
-
-from OpenGL.GL import *
-from OpenGL.GLU import *
+from imports import *
 
 vertices = (
     (1, -1, -1), (1, 1, -1), (-1, 1, -1), (-1, -1, -1),
