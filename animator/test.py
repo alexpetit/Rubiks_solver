6b2972b6afdf3d42867f43e9ecb4e54490982ecb
louiseslb <louise.saliba99@gmail.com>
2021-10-27 09:53:32 +0200

    touches to press mix

--- animator/test.py
+++ animator/test.py
@@ -20,11 +20,11 @@ solution = utils.solve(cube, 'Kociemba')
 
 kociemba_output = ["R'", "R", "L'", "L", "D'", "D", "U'", "U", "B'", "B", "F'", "F"]
 
-keypad_code = [K_1, K_F1, K_3, K_F3, K_4, K_F4, K_6, K_F6, K_7, K_F7, K_9, K_F9]
+keypad_code = ["K_1", "K_F1", "K_3", "K_F3", "K_4", "K_F4", "K_6", "K_F6", "K_7", "K_F7", "K_9", "K_F9"]
 
 kociemba_output_double = ["R2", "L2", "D2", "U2", "B2", "F2"]
 
-keypad_code_double = [K_1, K_3, K_4, K_6, K_7, K_9]
+keypad_code_double = ["K_1", "K_3", "K_4", "K_6", "K_7", "K_9"]
 
 touches_to_press_solve = []
 
@@ -38,23 +38,21 @@ for s in solution:
         touches_to_press_solve.append(keypad_code_double[index])
 
 print(solution)
-
 print(touches_to_press_solve)
 
 reversed_touches = reversed(touches_to_press_solve)
 
-F = [K_F1, K_F3, K_F4, K_F6, K_F7, K_F9]
+F = ["K_F1", "K_F3", "K_F4", "K_F6", "K_F7", "K_F9"]
 
-touches_to_press_mix = []
+result = []
 
 for touch in reversed_touches:
     if touch in F:
         index = F.index(touch)
-        touches_to_press_mix.append(keypad_code_double[index])
+        result.append(keypad_code_double[index])
     else:
         index = keypad_code_double.index(touch)
-        touches_to_press_mix.append(F[index])
+        result.append(F[index])
 
-print(touches_to_press_mix)
 
 
